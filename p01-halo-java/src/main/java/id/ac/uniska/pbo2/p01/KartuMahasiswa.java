0dda28d0cce26cf2ed6fb9538623a65d20ef78a9
M. Ridho Fadillah <ridhofadillahjer@gmail.com>
2026-09-24 05:26:39 +0800

    P01: tambah program HaloPBO2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,22 @@
+package id.ac.uniska.pbo2.p01;
+
+public class KartuMahasiswa {
+
+    public static void main(String[] args) {
+        String nama = "M. Ridho Fadillah";
+        String npm = "2410010166";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin meningkatkan kemampuan pemrograman berbasis objek";
+
+        System.out.println("==============================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("==============================");
+    }
+}
